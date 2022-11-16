a3d595cbd68013cee0e5985cbbf6aad4fa47537f
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-11-16 17:13:18 +0100

    feat: :sparkles: add unasm cli

---
+++ README.md
@@ -0,0 +1,34 @@
+# unasm.ts cli & repl
+
+## building
+
+1. clone the monorepo
+```bash
+git clone --recurse-submodules -j8 git@github.com:unasmlang/monorepo.git
+```
+2. cd into the cli dir
+```bash
+cd ts/cli
+```
+3. setup [libunasm](https://github.com/unasmlang/libunasm-ts)
+```bash
+pnpm update-lang
+```
+4. build
+```bash
+pnpm build
+```
+5. package
+```
+pnpm package
+```
+6. done - there's now a binary in the repo you can run
+
+## usage
+### repl
+1. run the executable
+2. type ur code (remember, if you have any jmp's or lbl's, you must jmp to ones defined previously in the same line! line history is NOT preserved cross-line)
+3. profit
+### cli
+1. run the executable, add the (.unasm) file as the 2nd argument
+2. profit
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as libunasm from './libunasm/dist/インデックス';
+import * as fs from 'fs';
+import * as readline from 'readline/promises';
+import {
+  stdin as input, stdout as output
+} from 'process';
+
+// since we're in nodejs, replace console
+libunasm.instructions.out = ()=>process.stdout.write(libunasm.registers.r1.toString());
+libunasm.instructions.outc = ()=>process.stdout.write(String.fromCharCode(libunasm.registers.r1));
+
+const fileName = process.argv[2];
+if (!fileName || !fs.existsSync(fileName)) {
+  // repl
+  const rl = readline.createInterface({
+    input, output
+  });
+  console.log(`libunasm.ts version ${libunasm.version}`);
+  console.log('welcome to the unasm repl');
+  console.log('excluding instruction number (& therefor jmps across lines), the state persists across script execution.');
+  console.log('use ctrl+c to exit.');
+  (async () => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const ans = await rl.question('> ');
+      console.log(libunasm.execute(ans));
+    }
+  })();
+}
+else
+  libunasm.execute(fs.readFileSync(process.argv[2], 'utf-8'));
